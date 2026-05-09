55b4709ce8cb7936260375dc0d303c9289e18e30
themannyc <mcamorling@gmail.com>
2026-05-09 12:14:12 -0700

    Update insert_data.sql

--- insert_data.sql
+++ insert_data.sql
@@ -1,37 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: May 09, 2026 at 05:41 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `cpsc_332_project`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `course`
---
-
-
-
---
--- Dumping data for table `course`
---
 
 INSERT INTO `course` (`course_num`, `title`, `textbook`, `units`, `dept_num`) VALUES
 (101, 'Introduction to Programming', 'Python Basics', 3, 10),
